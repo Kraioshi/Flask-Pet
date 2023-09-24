06fb17d4cc02b768d8ab9ece58cc337b2048fa69
Turgay <turgaj.abdullajev@gmail.com>
2023-09-24 23:58:40 +0400

    addd route for base page

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
+import requests
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('cat.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
