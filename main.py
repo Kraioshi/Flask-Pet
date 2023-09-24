138119f4a9edb430d3a078fca6b620e34daa3ed3
Turgay <turgaj.abdullajev@gmail.com>
2023-09-25 00:51:57 +0400

    got dog api key and shiba image

--- main.py
+++ main.py
@@ -1,25 +1,41 @@
 from flask import Flask, render_template
+from flask_bootstrap import Bootstrap5
 from dotenv import load_dotenv
 import os
 import requests
 
 app = Flask(__name__)
-
+Bootstrap5(app)
 load_dotenv()
 
-api_key = os.getenv('CAT')
-url = 'https://api.thecatapi.com/v1/images/search'
+cat_api_key = os.getenv('CAT')
+dog_api_key = os.getenv('DOG')
+cat_url = 'https://api.thecatapi.com/v1/images/search'
+dog_url = 'https://api.thedogapi.com/v1/images/search'
+shiba_url = 'http://shibe.online/api/shibes?count=[1]&urls=[true]&httpsUrls=[true]'
 
 headers = {
-    'Authorization': f'Bearer {api_key}',
+    'Authorization': f'Bearer {cat_api_key}',
 }
+
+
+shiba_response = requests.get(url=shiba_url).json()[0]
+shiba_image = f'https://cdn.shibe.online/shibes/{shiba_response}.jpg'
 
 
 @app.route('/')
 def index():
-    response = requests.get(url, headers=headers).json()
-    cat_url = response[0]['url']
-    return render_template('cat.html', cat_url=cat_url)
+    return render_template('index.html')
+
+# @app.route('/cat')
+# def cat():
+#     response = requests.get(url, headers=headers).json()
+#     cat_url = response[0]['url']
+#     return render_template('cat.html', cat_url=cat_url)
+#
+# @app.route('/shiba')
+# def shiba():
+#
 
 
 if __name__ == '__main__':
