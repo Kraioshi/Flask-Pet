7b58f636fb5b131d1d09ea5c63849ec1d62c8d81
Turgay <turgaj.abdullajev@gmail.com>
2023-09-25 00:04:39 +0400

    set API key

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from flask import Flask, render_template
+from dotenv import load_dotenv
+import os
 import requests
 
 app = Flask(__name__)
+
+load_dotenv()
+
+cat_key = os.getenv('CAT')
 
 
 @app.route('/')
